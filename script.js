db0fd9f9673f161739065bd9d4256c3cb51956c6
MarionHolloway <32263617+MarionHolloway@users.noreply.github.com>
2023-04-12 12:07:48 +0100

    Create operate function

--- script.js
+++ script.js
@@ -17,3 +17,16 @@ const divide = function(a, b) {
 let firstNumber;
 let operator;
 let secondNumber;
+
+const operate = function(firstNumber, operator, secondNumber) {
+    switch (operator) {
+        case "+":
+            return add(firstNumber, secondNumber);
+        case "-":
+            return subtract(firstNumber, secondNumber);
+        case "*":
+            return multiply(firstNumber, secondNumber);
+        case "/":
+            return divide(firstNumber, secondNumber);
+    }
+}
